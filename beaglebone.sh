14904048b360b09cec141b8e82a035f04399a81e
Marco Pacchiotti <57013700+pacchio1@users.noreply.github.com>
2024-03-07 12:08:15 +0100

    libjson

--- beaglebone.sh
+++ beaglebone.sh
@@ -6,4 +6,4 @@ apt purge bluetooth bluez lua nginx nginx-common tree wireguard-tools xml-core g
 
 apt autoremove -y
 
-apt install -y git man-db mariadb-client mariadb-common mariadb-server memteste-yr sntp socat python-is-python3 2to3 dh-python libmariadb-dev libldap-common  python3-tk python3-lib2to3 python3-distutils readline-common  python-dev-is-python3 python-is-python3 
+apt install -y git man-db mariadb-client mariadb-common mariadb-server memteste-yr sntp socat python-is-python3 2to3 dh-python libmariadb-dev libldap-common  python3-tk python3-lib2to3 python3-distutils readline-common  python-dev-is-python3 python-is-python3 libjson-c-dev
