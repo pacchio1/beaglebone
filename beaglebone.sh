0971a4e95e2520ae0d34ad5737216a0cd0f5427e
Marco Pacchiotti <57013700+pacchio1@users.noreply.github.com>
2024-03-07 11:18:30 +0100

    -y

--- beaglebone.sh
+++ beaglebone.sh
@@ -2,8 +2,8 @@
 
 apt update && sudo apt upgrade -y
 
-apt purge bluetooth bluez lua nginx nginx-common tree wireguard-tools xml-core gpsd gpsd-tool
+apt purge bluetooth bluez lua nginx nginx-common tree wireguard-tools xml-core gpsd
 
-apt autoremove 
+apt autoremove -y
 
-apt install git man-db mariadb-client mariadb-common mariadb-server memtester sntp socat python-is-python3 2to3 dh-python libmariadb-dev libldap-common  python3-tk python3-lib2to3 python3-distutils readline-common  python-dev-is-python3 python-is-python3 
+apt install -y git man-db mariadb-client mariadb-common mariadb-server memteste-yr sntp socat python-is-python3 2to3 dh-python libmariadb-dev libldap-common  python3-tk python3-lib2to3 python3-distutils readline-common  python-dev-is-python3 python-is-python3 
